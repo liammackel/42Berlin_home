2a96f90cb14e55f39d044ae0348c9aaf592bf584
liammackel <75731538+liammackel@users.noreply.github.com>
2023-10-02 20:32:14 +0200

    ex03 final2

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -6,12 +6,11 @@
 /*   By: liammackel <liammackel@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:56:04 by liammackel        #+#    #+#             */
-/*   Updated: 2023/10/02 20:30:52 by liammackel       ###   ########.fr       */
+/*   Updated: 2023/10/02 20:31:54 by liammackel       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void    ft_div_mod(int a, int b, int *div, int *mod)
 {   
