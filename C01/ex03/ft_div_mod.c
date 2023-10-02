f8d571025228d84a003aef53200ef7c00c1a556b
liammackel <75731538+liammackel@users.noreply.github.com>
2023-10-02 20:30:52 +0200

    ex03 final

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -6,7 +6,7 @@
 /*   By: liammackel <liammackel@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 19:56:04 by liammackel        #+#    #+#             */
-/*   Updated: 2023/10/02 20:27:39 by liammackel       ###   ########.fr       */
+/*   Updated: 2023/10/02 20:30:52 by liammackel       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,12 @@ void    ft_div_mod(int a, int b, int *div, int *mod)
 {   
     if (b != 0)
     {
-        div = a / b;
-        mod = a % b;
+        *div = a / b;
+        *mod = a % b;
     }
     else
     {
         *div = 0;
         *mod = 0;
     }
-}
-
-int main(void)
-{
-    printf("===== ex03 =====\n");
-    int a = 13;
-    int b = 5;
-    int div = -1;
-    int mod = -1;
-
-    ft_div_mod(a, b, &div, &mod);
-    printf("%d is divided by %d \nRESULT: %d\nREMAINDER: %d\n", a, b, div, mod);
 }
